6fc387175de201b68466636d35e34d77875c2de0
Michel <michelhoca@hotmail.com>
2016-05-15 14:24:20 +0200

    creacion fichero java

---
+++ hola.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hola;
+
+/**
+*
+* java.java
+*
+* Creada el 15-may-2016, 14:22:13
+*
+* Desarrollada por Miguel Hocasar en la empresa Abastos el dia 15-may-2016
+*
+* Puede contactar conmigo en michelhoca@hotmail.com
+*
+/**
+*
+* @author Miguel Hocasar
+* @version 1.0
+* @date 15-may-2016 
+ */
+public class java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
